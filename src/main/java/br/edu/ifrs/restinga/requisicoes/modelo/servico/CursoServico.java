156462d90cddfb9edccf37d65501862e643282ff
Jader Moraes de Moura <jader.mmoura@gmail.com>
2020-04-11 01:57:14 -0300

    criado metodo de apagar disciplina

--- src/main/java/br/edu/ifrs/restinga/requisicoes/modelo/servico/CursoServico.java
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/modelo/servico/CursoServico.java
@@ -1,6 +1,7 @@
 package br.edu.ifrs.restinga.requisicoes.modelo.servico;
 
 import br.edu.ifrs.restinga.requisicoes.modelo.dao.CursoDao;
+import br.edu.ifrs.restinga.requisicoes.modelo.dao.DisciplinaDao;
 import br.edu.ifrs.restinga.requisicoes.modelo.entidade.Curso;
 import br.edu.ifrs.restinga.requisicoes.modelo.entidade.Disciplina;
 import br.edu.ifrs.restinga.requisicoes.modelo.rn.CursoRN;
@@ -12,41 +13,56 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-
 @Service
-public class CursoServico extends ServicoCRUD<Curso>{
+public class CursoServico extends ServicoCRUD<Curso> {
 
     @Autowired
-    private  CursoDao dao;
+    private CursoDao dao;
     @Autowired
     private CursoRN rn;
     @Autowired
     private DisciplinaServico disciplinaServico;
+    
+    @Autowired
+    private DisciplinaDao disciplinaDao;
 
     @Override
     public CrudRepository<Curso, Long> getDAO() {
-          return  dao;
+        return dao;
     }
 
     @Override
     public RegraNenocio<Curso> rn() {
         return rn;
     }
-    
-    
-    public ResponseEntity<List<Disciplina>> cadastrarDisciplinaNoCurso(Long id, Disciplina d){
+
+    public ResponseEntity<List<Disciplina>> cadastrarDisciplinaNoCurso(Long id, Disciplina d) {
         Curso curso = super.recuperar(id).getBody();
         curso.getDisciplinas().add(d);
         Curso cursoRetorno = dao.save(curso);
-        return new ResponseEntity(cursoRetorno.getDisciplinas(),HttpStatus.CREATED);
+        return new ResponseEntity(cursoRetorno.getDisciplinas(), HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<Curso> atualizar(Curso entidade) {
-        
-        return super.atualizar(entidade); 
+        return super.atualizar(entidade);
     }
 
-    
+    public ResponseEntity<Curso> listarDisciplinas(Long id) {
+        Curso curso = super.recuperar(id).getBody();
+        return new ResponseEntity(curso.getDisciplinas(), HttpStatus.OK);
+    }
 
+    public void deletarDisciplina(Long id, Long idDisciplina) {
+        Curso curso = super.recuperar(id).getBody();
+        List<Disciplina> disciplinas = (List<Disciplina>) disciplinaDao.findAll();
+        System.out.println(id);
+                for (Disciplina disciplina : disciplinas) {
+            if (disciplina.getId() == idDisciplina) {
+        System.out.println(disciplina.getId());
+                curso.getDisciplinas().remove(disciplina);
+            }
+        }
+        dao.save(curso);
+    }
 }
