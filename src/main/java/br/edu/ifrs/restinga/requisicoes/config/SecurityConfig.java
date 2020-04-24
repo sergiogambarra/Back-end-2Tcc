44712b797028ec2a7798266642e4b7b2f7a033f0
unknown <jader.mmoura@gmail.com>
2020-04-24 01:08:58 -0300

    atualizar senha

--- src/main/java/br/edu/ifrs/restinga/requisicoes/config/SecurityConfig.java
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/api/usuarios/auth/","/api/requisicoes/solicitante/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/api/usuarios/**").hasAnyRole("SERVIDOR","PROFESSOR")
                 .antMatchers(HttpMethod.PUT,"/api/cursos/**").hasRole("SERVIDOR")
-                .antMatchers(HttpMethod.PUT,"/api/requisicoes/**").hasRole("SERVIDOR")
+                .antMatchers(HttpMethod.PUT,"/api/requisicoes/**").hasAnyRole("SERVIDOR","PROFESSOR")
                 .antMatchers(HttpMethod.GET,"/api/cursos/").hasAnyRole("ALUNO","SERVIDOR","PROFESSOR")
                 .antMatchers(HttpMethod.POST,"/api/requisicoes/** ").hasRole("ALUNO")
                 .antMatchers().permitAll()
--- src/main/java/br/edu/ifrs/restinga/requisicoes/modelo/servico/UsuarioServico.java
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/modelo/servico/UsuarioServico.java
@@ -25,10 +25,10 @@ public class UsuarioServico extends ServicoCRUD<Usuario> implements UserDetailsS
 
     @Autowired
     private UsuarioDao dao;
-    
+
     @Autowired
     private PerfilServico servicoPerfilServico;
-    
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -76,46 +76,48 @@ public class UsuarioServico extends ServicoCRUD<Usuario> implements UserDetailsS
     }
 
     @Transactional
-    public Usuario pesquisaLogin(String nome){
+    public Usuario pesquisaLogin(String nome) {
         return dao.findByUserName(nome);
-   }
+    }
 
     public List<Usuario> listarAluno() {
         Iterable<Usuario> usuarios = dao.findAll();
         ArrayList<Usuario> alunos = new ArrayList<>();
         for (Usuario usuario : usuarios) {
-            if(usuario.getPerfil().getTipo().equals("ALUNO")){
+            if (usuario.getPerfil().getTipo().equals("ALUNO")) {
                 alunos.add(usuario);
             }
         }
         System.out.println(alunos);
-                return alunos; 
+        return alunos;
     }
+
     public List<Usuario> listarProfessor() {
         Iterable<Usuario> usuarios = dao.findAll();
         ArrayList<Usuario> professores = new ArrayList<>();
         for (Usuario usuario : usuarios) {
-            if(usuario.getPerfil().getTipo().equals("PROFESSOR")){
+            if (usuario.getPerfil().getTipo().equals("PROFESSOR")) {
                 professores.add(usuario);
             }
         }
-                return professores; 
+        return professores;
     }
+
     public List<Usuario> listarServidor() {
         Iterable<Usuario> usuarios = dao.findAll();
         ArrayList<Usuario> servidores = new ArrayList<>();
         for (Usuario usuario : usuarios) {
-            if(usuario.getPerfil().getTipo().equals("SERVIDOR")){
+            if (usuario.getPerfil().getTipo().equals("SERVIDOR")) {
                 servidores.add(usuario);
             }
         }
-        return servidores; 
+        return servidores;
     }
 
     @Override
     public Usuario atualizar(Usuario entidade) {
         Usuario usuarioAntigo = recuperar(entidade.getId());
-        
+
         if (entidade.getPerfil() instanceof PerfilAluno) {
             PerfilAluno aluno = (PerfilAluno) entidade.getPerfil();
             PerfilAluno perfilAlunoAntigo = (PerfilAluno) usuarioAntigo.getPerfil();
@@ -123,20 +125,25 @@ public class UsuarioServico extends ServicoCRUD<Usuario> implements UserDetailsS
             perfilAlunoAntigo.setEmail(aluno.getEmail());
             perfilAlunoAntigo.setMatricula(aluno.getMatricula());
             perfilAlunoAntigo.setDataIngresso(aluno.getDataIngresso());
-        }else if (entidade.getPerfil() instanceof PerfilProfessor) {
-            PerfilProfessor professor = ( PerfilProfessor ) entidade.getPerfil();
+        } else if (entidade.getPerfil() instanceof PerfilProfessor) {
+            PerfilProfessor professor = (PerfilProfessor) entidade.getPerfil();
             PerfilProfessor perfilProfessor = (PerfilProfessor) usuarioAntigo.getPerfil();
             perfilProfessor.setNome(professor.getNome());
             perfilProfessor.setCordenador(professor.isCordenador());
             perfilProfessor.setSiape(professor.getSiape());
-        }else if(entidade.getPerfil() instanceof PerfilServidor){
+        } else if (entidade.getPerfil() instanceof PerfilServidor) {
             PerfilServidor servidor = (PerfilServidor) entidade.getPerfil();
             PerfilServidor perfilServidor = (PerfilServidor) usuarioAntigo.getPerfil();
             perfilServidor.setNome(servidor.getNome());
             perfilServidor.setSiape(servidor.getSiape());
         }
         perfilServico.salvarPerfil(usuarioAntigo);
-        return  usuarioAntigo;
+        return usuarioAntigo;
     }
-    
+
+    public Usuario atualizarSenha(Usuario u) {
+        Usuario usuarioAntigo = recuperar(u.getId());
+        usuarioAntigo.setPassword(passwordEncoder.encode(u.getPassword()));
+        return dao.save(usuarioAntigo);
+    }
 }
