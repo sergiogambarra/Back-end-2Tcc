178461e410f4a47e7f0e80c54cc4aacb1d669328
unknown <jader.mmoura@gmail.com>
2020-06-14 23:27:16 -0300

    colocado coordenador em curso

--- src/main/java/br/edu/ifrs/restinga/requisicoes/modelo/entidade/Curso.java
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/modelo/entidade/Curso.java
@@ -8,20 +8,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "cursos")
-public class Curso implements Entidade{
+public class Curso implements Entidade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,9 +30,10 @@ public class Curso implements Entidade{
 
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-     
+
     @OrderBy(value = "nome")
     private List<Disciplina> disciplinas;
 
-
+    @OneToOne
+    private Usuario usuario;
 }
