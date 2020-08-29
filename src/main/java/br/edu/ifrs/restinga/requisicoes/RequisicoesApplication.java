ec8f1332251fa8d4cb8c69a4d069131d4c273271
unknown <jader.mmoura@gmail.com>
2020-08-29 17:43:46 -0300

    testando importação csv

--- src/main/java/br/edu/ifrs/restinga/requisicoes/RequisicoesApplication.java
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/RequisicoesApplication.java
@@ -1,5 +1,7 @@
 package br.edu.ifrs.restinga.requisicoes;
 
+import java.io.File;
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,20 @@ public class RequisicoesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RequisicoesApplication.class, args);
+               
+                File arquivoCsv = new File("C:\\Users\\jader\\Área de Trabalho\\TCC\\cursos.csv");
+                
+                try {
+                    String linhasArquivo = new String();
+                    Scanner leitor = new Scanner(arquivoCsv);
+                    while(leitor.hasNext()){
+                linhasArquivo = leitor.nextLine();
+                System.out.println(linhasArquivo);
+                        
+                    }
+                            
+            } catch (Exception e) {
+            };
 	}
+        
 }
