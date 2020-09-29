91af3b979e9828635de3530410609e25f565268a
unknown <jader.mmoura@gmail.com>
2020-09-29 19:48:10 -0300

    importador disciplina associando curso

--- src/main/java/br/edu/ifrs/restinga/requisicoes/controle/AnexoControle.java
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/controle/AnexoControle.java
@@ -7,9 +7,7 @@ import br.edu.ifrs.restinga.requisicoes.modelo.servico.ServicoCRUD;
 import io.swagger.annotations.Api;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,10 +44,4 @@ public class AnexoControle extends CRUDControle<Anexo> {
         importacaoCsvServico.ImportarUsuarios(arquivo);
     }
     
-    
-//      @PostMapping(value = "/usuarios", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    public void importarUsuarios(@RequestParam("umArquivo") MultipartFile arquivo) throws IOException {
-//        System.out.println(arquivo.getOriginalFilename());
-//        importacaoCsvServico.ImportarUsuarios(arquivo);
-//    }
 }
