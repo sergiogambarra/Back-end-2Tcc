10ffaedfcf57360b8ab5b3730bd2398ebf16810b
unknown <jader.mmoura@gmail.com>
2020-10-27 19:48:28 -0300

    alterações class usuarioTest

--- src/test/java/br/edu/ifrs/restinga/requisicoes/modelo/entidade/RequisicaoTest.java
+++ src/test/java/br/edu/ifrs/restinga/requisicoes/modelo/entidade/RequisicaoTest.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.ifrs.restinga.requisicoes.modelo.entidade;
-
 import java.util.Date;
 import java.util.List;
 import org.junit.After;
@@ -14,10 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author jader
- */
 public class RequisicaoTest {
 
     RequisicaoAproveitamento requisição = new RequisicaoAproveitamento();
